267d5d6df1367a74c7c64068acb272c5b4fc5b05
Morlintie <ahmetyasintoraman@gmail.com>
2025-03-06 19:58:45 +0300

    first initialization

--- app.js
+++ app.js
@@ -6,13 +6,69 @@ const app = express();
 app.use(express.static("./public"));
 app.use(express.json());
 
-app.post("/test", async (req, res) => {
+app.get("/api/tasks/get", async (req, res) => {
   try {
-    console.log(req.body);
-    let test = await Task.create(req.body);
-    res.status(201).json(test);
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks: tasks });
   } catch (err) {
-    console.log(err);
+    res.status(500).json({ msg: err });
+  }
+});
+
+app.get("/api/tasks/get/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const thatTask = await Task.findOne({ _id: id });
+    if (thatTask) {
+      res.status(200).json({ task: thatTask });
+    } else {
+      res.status(404).json({ msg: "The task doesn't exists" });
+    }
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
+});
+
+app.post("/api/tasks/create", async (req, res) => {
+  try {
+    let task = await Task.create(req.body);
+    res.status(201).json({ task: task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
+});
+
+app.delete("/api/tasks/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const thatTask = await Task.findOneAndDelete({ _id: id });
+    if (thatTask) {
+      res
+        .status(200)
+        .json({ success: true, msg: "The task is successfully removed" });
+    } else {
+      res.status(404).json({ success: false, msg: "Task doesn't exists" });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, msg: error });
+  }
+});
+
+app.patch("/api/tasks/patch/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const thatTask = await Task.findOneAndUpdate({ _id: id }, req.body, {
+      new: true,
+      runValidator: true,
+    });
+    if (thatTask) {
+      res.status(200).json({ thatTask });
+    } else {
+      res.status(404).json({ success: false, msg: "The task doesn't exist" });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, msg: error });
   }
 });
 
--- db/connect.js
+++ db/connect.js
@@ -8,8 +8,16 @@ mongoose
   });
 
 const taskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide name"],
+    trim: true,
+    maxlength: [20, "name cannot be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const Task = mongoose.model("Task", taskSchema);
